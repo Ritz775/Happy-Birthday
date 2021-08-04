38bd89a613d1c870622abb5ba75e67a49d51c2ff
Ritz775 <82327782+Ritz775@users.noreply.github.com>
2021-08-04 13:56:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var canvas = new fabric.Canvas("myCanvas");
+var x = document.getElementById("myAudio");
+
+function new_image() {}
+
+function Playsound() {
+  x.play();
+  console.log("Happy Birthday To You!");
+}
